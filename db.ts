c655d202f0910783f18f70e3cc5f68eb74f010d7
NoSkhil <nshyamakhil@gmail.com>
2024-03-09 22:28:25 -0500

    moved db connectionString to env

--- db.ts
+++ db.ts
@@ -1,7 +1,6 @@
 import { Client } from 'pg';
 
-// Replace 'your_supabase_connection_url' with your actual Supabase connection URL
-const supabaseUrl = `postgres://postgres.tguiudddfjvlibhkjuja:${process.env.PGPASSWORD}@aws-0-ap-south-1.pooler.supabase.com:5432/postgres`;
+const supabaseUrl = process.env.PGURL;
 
 const client = new Client({
   connectionString: supabaseUrl
